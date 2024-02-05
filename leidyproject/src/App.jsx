6e3837eb5229a15a590d837a2c7f5f606da24157
leidy <leidy@DESKTOP-452UI5F>
2024-02-05 04:59:02 -0800

    third tab

--- leidyproject/src/App.jsx
+++ leidyproject/src/App.jsx
@@ -3,16 +3,37 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+
+function handleClick(pupId){
+  setFeatPupId(pupId);
+}
+
+const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+console.log(featuredPup);
 
   return (
     <>
     <div className ="App">
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
+      {puppies.map((puppy) => {
+          return (<p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+            </p>
+          );
         })
-      }
+      };
+      
     </div>
     </>
   );
